0286327379b7e1a3da7247c358da855e83c0d148
Brandon Moran <BrandonEM1@comcast.net>
2018-08-18 15:07:07 -0700

    layout done, need to fix stuff

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var friendsArray = [
+	{
+	  "name":"Keanu Reeves",
+	  "photo":"https://cdn-2.returnofthecaferacers.com/wp-content/uploads/2018/02/keanu-reeves-motorcycle-8-e1528096785376.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	},
+	{
+	  "name":"Dave Grohl",
+	  "photo":"https://consequenceofsound.files.wordpress.com/2015/03/screen-shot-2015-03-16-at-1-28-10-pm.png?w=795",
+	  "scores":[
+	     1,
+	     2,
+	     5,
+	     5,
+	     1,
+	     2,
+	     3,
+	     1,
+	     5,
+	     2
+	   ]
+	},
+	{
+	  "name":"Stevie Nicks",
+	  "photo":"https://igetjokes.files.wordpress.com/2015/03/fleetwood-mac-bcvb_o_tn.jpg",
+	  "scores":[
+	     4,
+	     5,
+	     3,
+	     3,
+	     4,
+	     5,
+	     1,
+	     4,
+	     3,
+	     5
+	   ]
+	},
+	{
+	  "name":"Elon Musk",
+	  "photo":"https://www.telegraph.co.uk/content/dam/technology/2018/05/03/TELEMMGLPICT000162148580_trans_NvBQzQNjv4Bq_Qt1iuvWth76e9d_IW2U0SeeW-B59ySlNOPo8_i5J48.jpeg?imwidth=450",
+	  "scores":[
+	     2,
+	     3,
+	     1,
+	     1,
+	     2,
+	     3,
+	     4,
+	     2,
+	     1,
+	     3
+	   ]
+	}
+];
+
+// Set up table to be accessible to other files using require.
+module.exports = friendsArray;
